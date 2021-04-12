123c4bc08ff33cd227c6db8dd4152699b3fd5cc1
Guillaume Masclet <guillaume@masclet.net>
2021-04-12 22:42:10 +0200

    Add a deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+
+SSH_USER=guillaume
+SSH_HOST=masclet.net
+REMOTE_PATH=/home/guillaume/www/nginx-proxy/
+
+rsync -av --delete \
+    --exclude=deploy.sh \
+    --exclude=.git \
+    --exclude=.env \
+    --exclude=*.dist \
+    --exclude=htpasswd/* \
+    ./ "$SSH_USER@$SSH_HOST:$REMOTE_PATH"
